e3d1f8feb43a5906945db6857890e70ec050a853
Thangapandittp <thangapandittp@gmail.com>
2020-12-25 17:59:01 +0530

    ok

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from river.models.fields.state import StateField
 
 
-class Ticket(models.Model):
+class Ticket(models.Model):  
     no = models.CharField("Ticket Number", max_length=50, default=uuid.uuid4, null=False, blank=False, editable=False,
                           unique=True)
     subject = models.CharField("Subject", max_length=100, null=False, blank=False)
